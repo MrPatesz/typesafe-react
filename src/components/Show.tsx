b3695ba1ede8087edf6089ead9968fb844dc3916
Patrik Ángyán <patesz1999@gmail.com>
2024-07-14 19:27:01 +0200

    Show refactored

--- src/components/Show.tsx
+++ src/components/Show.tsx
@@ -1,64 +1,40 @@
 import { type ReactNode } from 'react';
 import { type NonNullableElements } from '../types/nonNullableElements';
-import { type XOR } from '../types/xor';
+import { type IsEqual } from 'type-fest';
 
 /**
  * Component to conditionally render content.
  * @param when Single condition to evaluate.
  * @param whenAll Multiple conditions to evaluate.
  * @param fallback Content to render if 'when' is falsy or 'whenAll' contains some falsy elements.
- * @param children Content to render if 'when' is truthy or 'whenAll' only constains truthy elements.
- * @param childrenFn Callback that receives 'when' or 'whenAll' and returns content to render if 'when' is truthy or 'whenAll' only constains truthy elements.
+ * @param children Children callback that receives 'when' or 'whenAll' and returns content to render if 'when' is truthy or 'whenAll' only constains truthy elements.
  */
-export function Show<W>(
-  props: { when: W; fallback?: ReactNode } & XOR<
-    {
-      children: ReactNode;
-    },
-    {
-      childrenFn: (data: NonNullable<W>) => ReactNode;
-    }
-  >
-): ReactNode;
-
-export function Show<const T extends ReadonlyArray<unknown>>(
-  props: {
-    whenAll: T;
-    fallback?: ReactNode;
-  } & XOR<
-    {
-      children: ReactNode;
-    },
-    {
-      childrenFn: (data: NonNullableElements<T>) => ReactNode;
-    }
-  >
-): ReactNode;
-
-export function Show(props: {
-  when?: unknown;
-  whenAll?: ReadonlyArray<unknown>;
+export function Show<W = never, const T extends ReadonlyArray<unknown> = never>({
+  fallback,
+  children,
+  ...rest
+}: {
   fallback?: ReactNode;
-  children?: ReactNode;
-  childrenFn?: unknown;
-}): ReactNode {
-  if ('when' in props) {
-    if (!props.when) {
-      return props.fallback;
+  children: (
+    data: IsEqual<W, never> extends true ? NonNullableElements<T> : NonNullable<W>
+  ) => ReactNode;
+} & (
+  | {
+      when: W;
     }
-    if ('childrenFn' in props && props.childrenFn instanceof Function) {
-      return props.childrenFn(props.when);
+  | {
+      whenAll: T;
     }
-    return props.children;
-  } else if ('whenAll' in props && Array.isArray(props.whenAll)) {
-    if (props.whenAll.some((w) => !w)) {
-      return props.fallback;
+)) {
+  if ('when' in rest) {
+    if (!rest.when) {
+      return fallback;
     }
-    if ('childrenFn' in props && props.childrenFn instanceof Function) {
-      return props.childrenFn(props.whenAll);
-    }
-    return props.children;
+    return children(rest.when as Parameters<typeof children>[0]);
   } else {
-    throw new Error(`Neither "when" nor "whenAll" was provided!`);
+    if (rest.whenAll.some((w) => !w)) {
+      return fallback;
+    }
+    return children(rest.whenAll as Parameters<typeof children>[0]);
   }
 }
