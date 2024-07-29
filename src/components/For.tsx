37493941a3fb1adb726a2b3c2162e2992d763413
Patrik Ángyán <patesz1999@gmail.com>
2024-07-29 21:15:52 +0200

    For making use of children callback

--- src/components/For.tsx
+++ src/components/For.tsx
@@ -1,4 +1,4 @@
-import { Fragment, type ReactNode } from 'react';
+import { Fragment, type Key, type ReactNode } from 'react';
 import { type ConditionalProperties } from '../types/conditionalProperties';
 import { type IsNotEqual } from '../types/isNotEqual';
 
@@ -6,26 +6,26 @@ import { type IsNotEqual } from '../types/isNotEqual';
  * Component for rendering lists.
  * @param each Array to iterate over.
  * @param keyFn Function that returns the key for each item.
- * @param mapFn Function that returns the content for each item. No need to define key here.
+ * @param children Function that returns the content for each item. No need to define key here.
  * @param fallback Content to render if 'each' is nullish. Must be provided when 'each' is nullable.
  */
 export const For = <
-  E extends ReadonlyArray<unknown> | null | undefined,
+  E extends ReadonlyArray<I> | null | undefined,
   I = E extends ReadonlyArray<infer T> ? T : never,
 >({
   each,
   keyFn,
-  mapFn,
+  children,
   ...rest
 }: {
   each: E;
-  keyFn: (item: NoInfer<I>, index: number) => string | number;
-  mapFn: (item: NoInfer<I>, index: number) => ReactNode;
+  keyFn: (item: NoInfer<I>, index: number) => Key;
+  children: (item: NoInfer<I>, index: number) => ReactNode;
 } & ConditionalProperties<IsNotEqual<E, NonNullable<E>>, { fallback: ReactNode }>) => {
   if (each) {
-    return (each as ReadonlyArray<I>).map((item, index) => {
+    return each.map((item, index) => {
       const key = keyFn(item, index);
-      const element = mapFn(item, index);
+      const element = children(item, index);
       return <Fragment key={key}>{element}</Fragment>;
     });
   } else if ('fallback' in rest) {
