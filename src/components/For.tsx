a8f9eddca4daad220ebb74744eccca2c0d8b0263
Patrik Ángyán <patesz1999@gmail.com>
2024-07-29 22:12:18 +0200

    For improved

--- src/components/For.tsx
+++ src/components/For.tsx
@@ -1,36 +1,29 @@
 import { Fragment, type Key, type ReactNode } from 'react';
-import { type ConditionalProperties } from '../types/conditionalProperties';
-import { type IsNotEqual } from '../types/isNotEqual';
 
 /**
  * Component for rendering lists.
  * @param each Array to iterate over.
  * @param keyFn Function that returns the key for each item.
  * @param children Function that returns the content for each item. No need to define key here.
- * @param fallback Content to render if 'each' is nullish. Must be provided when 'each' is nullable.
+ * @param fallback Content to render if 'each' is empty.
  */
-export const For = <
-  E extends ReadonlyArray<I> | null | undefined,
-  I = E extends ReadonlyArray<infer T> ? T : never,
->({
+export const For = <I extends NonNullable<unknown>>({
   each,
   keyFn,
   children,
-  ...rest
+  fallback,
 }: {
-  each: E;
+  each: ReadonlyArray<I>;
   keyFn: (item: NoInfer<I>, index: number) => Key;
   children: (item: NoInfer<I>, index: number) => ReactNode;
-} & ConditionalProperties<IsNotEqual<E, NonNullable<E>>, { fallback: ReactNode }>) => {
-  if (each) {
-    return each.map((item, index) => {
-      const key = keyFn(item, index);
-      const element = children(item, index);
-      return <Fragment key={key}>{element}</Fragment>;
-    });
-  } else if ('fallback' in rest) {
-    return rest.fallback as ReactNode;
-  } else {
-    throw new Error(`"each" was nullish and "fallback" wasn't provided!`);
+  fallback?: ReactNode;
+}) => {
+  if (!each.length) {
+    return fallback;
   }
+  return each.map((item, index) => {
+    const key = keyFn(item, index);
+    const element = children(item, index);
+    return <Fragment key={key}>{element}</Fragment>;
+  });
 };
